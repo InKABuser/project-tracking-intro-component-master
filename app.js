610053e6084b499194bf75a647fb3ce20b4566df
InKABuser <71922550+InKABuser@users.noreply.github.com>
2024-10-13 23:30:59 +0300

    some media quieries

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ navToggle.addEventListener('click', () => {
      if (visibility === "false") {
         primaryNav.setAttribute('data-visible', true)
         navToggle.setAttribute('aria-expanded', true)
-        navToggle.style.backgroundimage = "url('images/icon-close.svg')";
      } else if (visibility === 'true') {
         primaryNav.setAttribute('data-visible', false)
         navToggle.setAttribute('aria-expanded', false)
-        navToggle.style.backgroundimage = "url('images/icon-hamburger.svg')";
      }
 })
